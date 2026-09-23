2844569e49e45dfc96545b78193339a4ed736130
KlevakKitchen <klevakkitchen@gmail.com>
2026-09-23 17:45:54 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -6,7 +6,7 @@ def payment_keyboard(book_key=None):
     keyboard = types.InlineKeyboardMarkup()
     if book_key:
         button = types.InlineKeyboardButton(
-            text="💳 Купить книгу за 1 ⭐",
+            text="💳 Купить за 1 ⭐",
             callback_data=f"buy_{book_key}"
         )
         keyboard.add(button)
